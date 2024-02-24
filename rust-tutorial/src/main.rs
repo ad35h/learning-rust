1a28a1c096161df41156fd7b9ef02264f1c1d7c0
Bhoovandev Fakooa (RAPP) <bhoovandev.fakooa@rapp.com>
2024-02-24 16:05:50 +0000

    Scope

--- rust-tutorial/src/main.rs
+++ rust-tutorial/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
     let x = 4;
     println!("x is : {}", x);
+
+    {
+        let x = 2;
+        println!("x is : {}", x);
+    }
+
+    let x = x + 1;
+    println!("x is : {}", x);
 }
